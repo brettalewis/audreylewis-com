f618f3cccc162fbdd467e40a696c4bb3734d2720
Brett Lewis <brett@pendo.io>
2025-09-27 18:18:27 -0400

    trying to clean up css

--- content/contact/contact-form.js
+++ content/contact/contact-form.js
@@ -12,19 +12,19 @@ window.onload = function () {
       }
     }).then(response => {
       if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
+        status.innerHTML = "<span class='success'>Thanks for your submission!</span>";
         form.reset()
       } else {
         response.json().then(data => {
           if (Object.hasOwn(data, 'errors')) {
             status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
           } else {
-            status.innerHTML = "Oops! There was a problem submitting your form"
+            status.innerHTML = "<span class='error'>Oops! There was a problem submitting your form</span>"
           }
         })
       }
     }).catch(error => {
-      status.innerHTML = "Oops! There was a problem submitting your form"
+      status.innerHTML = "<span class='error'>Oops! There was a problem submitting your form</span>"
     });
   }
   form.addEventListener("submit", handleSubmit)
